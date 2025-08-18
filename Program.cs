64e6ce5502ed9f40f6686c30ad21e92f61e32d32
4an <ivan.busko.zh@gmail.com>
2025-08-18 10:40:33 +0300

    courier

--- Program.cs
+++ Program.cs
@@ -1,422 +1,203 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
 
-
-internal class Program
+namespace DeliveryService
 {
-
-    public static void Main(string[] args)
+    // Абстрактный базовый класс для курьеров
+    public abstract class Courier
     {
-        //string address = "Peshkov str 75";
-        //double distans = 500;
-        //double weight = 10;
-        string[,] arreyCourier = new string[1,4];
-        string[,] arreyOrder;
-
-        AddCourier(ref arreyCourier,"Piter","Foot",5);
-        AddCourier(ref arreyCourier, "Alex", "Car", 4);
-
-
-
-
-        bool exitDo = true;
-        do
+        private string _name;
+        public string Name
         {
-            Console.WriteLine("Выберите действие\n1. работа с заказами\n2. работа с курьерами\n3. завершение работы");
-            double ans = CheckNum("Выберите действие",false);
-            if (ans == 1)
-            {
-                Console.WriteLine("Работа с заказами");
-                viewOrder();
-            }
-            else if (ans==2)
-            {
-                Console.WriteLine("Работа с курьерами");
-                viewСourier(arreyCourier);
-            }
-            else if (ans == 3)
-            {
-                Console.WriteLine("Работа завершена");
-                exitDo = false;
-            }
-            else
-            {
-                Console.WriteLine("Error");
+            get { return _name; }
+            set { _name = value; }
+        }
 
-            }
+        public abstract string Type { get; }
+        public abstract double MaxDistance { get; }
+        public abstract double AverageSpeed { get; }
+        public abstract double CarryingCapacity { get; }
 
-
-        } while (exitDo);
-
-    }
-
-    private static void viewOrder()     
-    {
-
-
-        bool exitDo = true;
-        do
+        public Courier(string name)
         {
-            Console.WriteLine("Выберите действие\n1. Список активных заказов\n2. Список завершенных заказов\n3. Список отказов\n4. Добавить заказ\n5. Удалить заказ\n6. Завершить заказ\n7. Возврат");
-            double ans = CheckNum("Выберите действие", false);
-            if (ans == 1)
-            {
-                
-            }
-            else if (ans == 2)
-            {
+            _name = name;
+        }
 
-            }
-            else if (ans == 3)
-            {
-                
-            }
-            else if (ans == 4)
-            {
-
-            }
-            else if (ans == 5)
-            {
-
-            }
-            else if (ans == 6)
-            {
-
-            }
-            else if (ans == 7)
-            {
-                exitDo = false;
-                Console.WriteLine("Возврат");
-            }
-            else
-            {
-                Console.WriteLine("Error");
-
-            }
-
-
-        } while (exitDo);
-
-    }
-
-    static void viewСourier(string[,] arreyCourier)
-    {
-
-
-        bool exitDo = true;
-        do
+        public virtual double? CalculateDeliveryTime(double distance)
         {
-            Console.WriteLine("Выберите действие\n1. Список курьров\n2. Добавить пешего\n3. Добавить вело\n4. Добавить авто\n5. Добавить грузо\n6. Удалить\n7. Возврат");
-            double ans = CheckNum("Выберите действие", false);
-            if (ans == 1)
+            if (distance <= MaxDistance)
             {
-                showArrey(arreyCourier);
+                return distance / AverageSpeed;
             }
-            else if (ans == 2)
-            {
-                СourierFoot courierFoot = new СourierFoot();
-            }
-            else if (ans == 3)
-            {
-                СourierFoot courierFoot = new СourierFoot();
-            }
-            else if (ans == 4)
-            {
+            return null;
+        }
 
-            }
-            else if (ans == 5)
-            {
-
-            }
-            else if (ans == 6)
-            {
-
-            }
-            else if (ans == 7)
-            {
-                exitDo = false;
-                Console.WriteLine("Возврат");
-            }
-            else
-            {
-                Console.WriteLine("Error");
-
-            }
-
-
-        } while (exitDo);
-
-    }
-
-    //abstract class Delivery
-    //{
-    //    public string Address
-    //    {
-    //        get 
-    //        { 
-    //            return Address;
-    //        }
-    //        set 
-    //        {
-    //            Address = CheckStr("введите адрес");
-
-    //        }
-
-    //    }
-
-    //    public double Distans 
-    //    {
-    //        get 
-    //        {
-    //            return Distans;
-    //        }
-    //        set 
-    //        {
-    //            Distans = CheckNum("введите растояние",false);
-
-    //        }
-    //    }
-
-    //    public double Weight
-    //    {
-    //        get
-    //        {
-    //            return Weight;
-    //        }
-    //        set
-    //        {
-    //            Distans = CheckNum("введите вес", false);
-
-    //        }
-    //    }
-
-    //}
-
-    //class HomeDelivery : Delivery
-    //{
-    //    public HomeDelivery(string address, double distans, double weight)
-    //    {
-    //        Address= address;
-    //        Distans= distans;
-    //        Weight= weight;
-
-    //    } 
-    //    public void DisplayParam
-
-    //}
-
-    //class PickPointDelivery : Delivery
-    //{
-    //    public PickPointDelivery(string address, double distans, double weight)
-    //    {
-    //        Address = address;
-    //        Distans = distans;
-    //        Weight = weight;
-
-    //    }
-    //}
-
-    //class ShopDelivery : Delivery
-    //{
-    //    public ShopDelivery(string address, double distans, double weight)
-    //    {
-    //        Address = address;
-    //        Distans = distans;
-    //        Weight = weight;
-
-    //    }
-    //}
-
-    abstract class Сourier
-    {
-        public string Name;
-        public void Courier(string name) 
+        public override string ToString()
         {
-            Name = CheckStr("Введите имя");
+            return $"{Type} {Name} (Макс. расстояние: {MaxDistance} км, Скорость: {AverageSpeed} км/ч, Грузоподъемность: {CarryingCapacity} кг)";
         }
     }
 
-    //class Сourieres : Сourier
-    //{   
-    //    protected string ID;
-    //    public string[,] arreyСourier;
-
-    //}
-
-    class СourierFoot : Сourier
+    // Конкретные типы курьеров
+    public class WalkingCourier : Courier
     {
-        protected double maxDistans = 1000;
-        protected double minWeght = 10;
-        protected double Velosity = 8;
-        
+        public override string Type { get { return "Пеший курьер"; } }
+        public override double MaxDistance { get { return 5; } }
+        public override double AverageSpeed { get { return 5; } }
+        public override double CarryingCapacity { get { return 5; } }
 
+        public WalkingCourier(string name) : base(name) { }
     }
 
-    class СourierBicycle : Сourier
+    public class BicycleCourier : Courier
     {
-        protected double maxDistans = 5000;
-        protected double minWeght = 20;
-        protected double Velosity = 16;
+        public override string Type { get { return "Велокурьер"; } }
+        public override double MaxDistance { get { return 20; } }
+        public override double AverageSpeed { get { return 15; } }
+        public override double CarryingCapacity { get { return 15; } }
 
-
+        public BicycleCourier(string name) : base(name) { }
     }
 
-    class СourierCar : Сourier
+    public class CarCourier : Courier
     {
-        protected double maxDistans = 100000;
-        protected double minWeght = 100;
-        protected double Velosity = 120;
+        public override string Type { get { return "Автокурьер"; } }
+        public override double MaxDistance { get { return 500; } }
+        public override double AverageSpeed { get { return 60; } }
+        public override double CarryingCapacity { get { return 50; } }
 
-
+        public CarCourier(string name) : base(name) { }
     }
 
-    class СourierTruck : Сourier
+    public class TruckCourier : Courier
     {
-        protected double maxDistans = 100000;
-        protected double minWeght = 10000;
-        protected double Velosity = 80;
+        public override string Type { get { return "Грузовой курьер"; } }
+        public override double MaxDistance { get { return 1000; } }
+        public override double AverageSpeed { get { return 80; } }
+        public override double CarryingCapacity { get { return 2000; } }
 
-
+        public TruckCourier(string name) : base(name) { }
     }
 
 
 
-
-    //class Order<TDelivery,
-    //TStruct> where TDelivery : Delivery
-    //{
-    //    public TDelivery Delivery;
-
-    //    public int Number;
-
-    //    public string Description;
-
-    //    public void DisplayAddress()
-    //    {
-    //        Console.WriteLine(Delivery.Address);
-    //    }
-
-    //    // ... Другие поля
-    //}
-
-    public static double CheckNum(string msg, bool checkZero)
+    // Служба доставки
+    public class DeliveryService
     {
-        //Console.WriteLine(msg);
-        string str;
-        bool exitDo = true;
-        double inum = 0;
-        do
+        private Courier[] _couriers;
+        private int _courierCount;
+        private int _orderCount;
+
+        public DeliveryService(int maxCouriers, int maxOrders)
         {
-            str = Console.ReadLine();
-
-            if (double.TryParse(str, out double num))
-            {
-                if (num == 0 && checkZero == true)
-                {
-                    Console.WriteLine("Error " + msg);
-                }
-                else
-                {
-                    exitDo = false;
-                    inum = num;
-
-                }
-            }
-            else
-            {
-                Console.WriteLine("Error " + msg);
-
-            }
-
-
-        } while (exitDo);
-        return inum;
-
-
-    }
-    static string CheckStr(string msg)
-    {
-        Console.WriteLine(msg);
-        string str;
-        bool exitDo = true;
-        do
-        {
-
-            str = Console.ReadLine();
-            if (int.TryParse(str, out int Num) || str == "")
-            {
-                Console.WriteLine("Error " + msg);
-            }
-            else
-            {
-                
-                exitDo = false;
-            }
-
-
-        } while (exitDo);
-
-        return str;
-    }
-
-    static void AddCourier(ref string[,] arreyCourier, string Name, string Type, int Rating)
-    {
-        //Console.WriteLine(arreyCourier.GetUpperBound(0));
-        //Console.WriteLine(arreyCourier.GetUpperBound(1));
-        if (arreyCourier[arreyCourier.GetUpperBound(0), 0] !=null)
-        {
-            
-        
-            string[,] arreyTemp = new string[arreyCourier.GetUpperBound(0) + 2, 4];
-            for (int i = 0; i <= arreyCourier.GetUpperBound(0); i++)
-            {
-                for (int j = 0; j <= arreyCourier.GetUpperBound(1); j++)
-                {
-                    arreyTemp[i, j] = arreyCourier[i, j];
-
-                }
-
-            }
-            arreyCourier = arreyTemp;
-        }
-        arreyCourier[arreyCourier.GetUpperBound(0), 0] = "CR" + (arreyCourier.GetUpperBound(0) + 1).ToString("0000");
-        arreyCourier[arreyCourier.GetUpperBound(0), 1] = Name;
-        arreyCourier[arreyCourier.GetUpperBound(0), 2] = Type;
-        arreyCourier[arreyCourier.GetUpperBound(0), 3] = Rating.ToString();
-
-        //showArrey(arreyCourier);
-
-
-    }
-
-    static void showArrey(string[,] Arrey)
-    {
-
-
-        Console.WriteLine("\nСписок");
-        for (int i = 0; i <= Arrey.GetUpperBound(0); i++)
-        {
-            string str = (i + 1).ToString() + ". ";
-                
-                
-            for (int j = 0; j <= Arrey.GetUpperBound(1); j++)
-            {
-
-                
-                str += Arrey[i, j]+" ";
-                
-
-            }
-            Console.WriteLine(str);
+            _couriers = new Courier[maxCouriers];
+            _courierCount = 0;
+            _orderCount = 0;
         }
 
-        Console.WriteLine();
+        public bool AddCourier(Courier courier)
+        {
+            if (_courierCount >= _couriers.Length)
+            {
+                Console.WriteLine("Достигнуто максимальное количество курьеров!");
+                return false;
+            }
+
+            _couriers[_courierCount++] = courier;
+            Console.WriteLine($"Добавлен курьер: {courier}");
+            return true;
+        }
+
+        public void ShowAllCouriers()
+        {
+            Console.WriteLine("\nСписок всех курьеров:");
+            for (int i = 0; i < _courierCount; i++)
+            {
+                Console.WriteLine(_couriers[i]);
+            }
+        }
 
     }
 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new DeliveryService(10, 50);
 
+            // Добавляем курьеров по умолчанию
+            service.AddCourier(new WalkingCourier("WC"));
+            service.AddCourier(new BicycleCourier("BC"));
+            service.AddCourier(new CarCourier("CC"));
+            service.AddCourier(new TruckCourier("TC"));
 
+            while (true)
+            {
+                Console.WriteLine("\nМеню:");
+                Console.WriteLine("1. Добавить курьера");
+                //Console.WriteLine("2. Создать заказ");
+                Console.WriteLine("3. Показать всех курьеров");
+                //Console.WriteLine("4. Показать все заказы");
+                Console.WriteLine("5. Выход");
 
+                Console.Write("Выберите действие: ");
+                string choice = Console.ReadLine();
 
+                switch (choice)
+                {
+                    case "1":
+                        AddNewCourier(service);
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        service.ShowAllCouriers();
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        return;
+                    default:
+                        Console.WriteLine("Неверный выбор!");
+                        break;
+                }
+            }
+        }
 
+        static void AddNewCourier(DeliveryService service)
+        {
+            Console.WriteLine("\nДобавление нового курьера:");
+            Console.WriteLine("1. Пеший курьер");
+            Console.WriteLine("2. Велокурьер");
+            Console.WriteLine("3. Автокурьер");
+            Console.WriteLine("4. Грузовой курьер");
+
+            Console.Write("Выберите тип курьера: ");
+            string typeChoice = Console.ReadLine();
+
+            Console.Write("Введите имя курьера: ");
+            string name = Console.ReadLine();
+
+            Courier courier = null;
+            switch (typeChoice)
+            {
+                case "1":
+                    courier = new WalkingCourier(name);
+                    break;
+                case "2":
+                    courier = new BicycleCourier(name);
+                    break;
+                case "3":
+                    courier = new CarCourier(name);
+                    break;
+                case "4":
+                    courier = new TruckCourier(name);
+                    break;
+                default:
+                    Console.WriteLine("Неверный выбор типа курьера!");
+                    return;
+            }
+
+            service.AddCourier(courier);
+        }
+    }
 }
